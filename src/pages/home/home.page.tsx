930e1c72e562e08c146e6073ba871d2555dde3e2
Nilton Sousa <nsousa@zak.app>
2023-02-16 09:58:57 -0300

    feat: create header page

---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,14 @@
+// Components
+import Header from "../../components/header/header.component";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+
+      <div className="App"></div>
+    </>
+  );
+};
+
+export default HomePage;

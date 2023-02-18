80eb53b957e278b84e5c8b8d02200d82607c6627
Nilton Sousa <nsousa@zak.app>
2023-02-18 10:43:05 -0300

    feat: add new register route on app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FunctionComponent } from "react";
 import HomePage from "./pages/home/home.page";
+import RegisterBillPage from "./pages/register/register.page";
 
 const App: FunctionComponent = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterBillPage />} />
       </Routes>
     </BrowserRouter>
   );

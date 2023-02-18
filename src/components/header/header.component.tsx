f87b98c0351872cb172bc43bc102a3f65deafcb6
Nilton Sousa <nsousa@zak.app>
2023-02-18 10:42:47 -0300

    feat: add action click on register button on header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,11 +1,18 @@
 // Styles
 import { HeaderContainer, HeaderItems, HeaderItem } from "./header.styles";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLRegisterClick = () => {
+    navigate("/register");
+  };
+
   return (
     <HeaderContainer>
       <HeaderItems>
-        <HeaderItem>Cadastrar</HeaderItem>
+        <HeaderItem onClick={handleLRegisterClick}>Cadastrar</HeaderItem>
         <HeaderItem>Relatórios</HeaderItem>
         <HeaderItem>Suporte</HeaderItem>
         <HeaderItem>Avalie</HeaderItem>
